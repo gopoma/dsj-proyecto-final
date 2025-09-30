d2c976f6eb800b9c07516beb4aa4141950ee433b
gopoma <gordono@unsa.edu.pe>
2025-09-29 20:56:41 -0500

    add: player's model

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Cinemachine;
+
+public class Player : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
